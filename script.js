387485e5d8b7bd26d07e7813e3b19043cc955377
Aniket Gauda <122367767+AniketGauda@users.noreply.github.com>
2024-05-19 22:43:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+let arr = ["Red","Green","White","Black"];
+let pDiv = document.getElementById("colorSelect");
+function display(){
+    pDiv.innerHTML = ``;
+    arr.forEach((elt,idx)=>{
+        let ch = document.createElement("option");
+        ch.textContent = `${elt}`;
+        ch.setAttribute("id",`opt-${idx}`);
+        pDiv.appendChild(ch);
+    });
+}
+
+display();
+
+function myFun(){
+    arr = arr.filter((elt)=>{
+        if(elt!=pDiv.value)
+            return elt;
+    })
+    display();
+}
